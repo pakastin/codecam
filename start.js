e283057f55a23922ca820bf30168d9168e1e9b8d
Juha Lindstedt <juha@pakastin.fi>
2016-10-22 21:47:52 +0300

    fixed install

---
+++ start.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+var cp = require('child_process')
+
+exec('npm', ['start'])
+
+function exec (cmd, args) {
+  var child = cp.spawn(cmd, args, { cwd: __dirname })
+
+  child.stdout.pipe(process.stdout)
+  child.stderr.pipe(process.stderr)
+}
