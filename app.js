f607daf20161f2fd0e9c06194f89500da48d375c
Juha Lindstedt <juha@pakastin.fi>
2016-10-24 21:57:52 +0300

    fixed menu

--- app.js
+++ app.js
@@ -4,11 +4,157 @@ const { app, dialog, BrowserWindow, Menu, MenuItem } = require('electron')
 
 let win
 
+const template = [
+  {
+    label: 'Edit',
+    submenu: [
+      {
+        role: 'undo'
+      },
+      {
+        role: 'redo'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'cut'
+      },
+      {
+        role: 'copy'
+      },
+      {
+        role: 'paste'
+      },
+      {
+        role: 'pasteandmatchstyle'
+      },
+      {
+        role: 'delete'
+      },
+      {
+        role: 'selectall'
+      }
+    ]
+  },
+  {
+    label: 'View',
+    submenu: [
+      {
+        role: 'togglefullscreen'
+      }
+    ]
+  },
+  {
+    role: 'window',
+    submenu: [
+      {
+        role: 'minimize'
+      }
+    ]
+  },
+  {
+    role: 'help',
+    submenu: [
+      {
+        label: 'Learn More',
+        click () { require('electron').shell.openExternal('https://github.com/pakastin/codecam') }
+      }
+    ]
+  }
+]
+
+if (process.platform === 'darwin') {
+  template.unshift({
+    label: app.getName(),
+    submenu: [
+      {
+        role: 'about'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'services',
+        submenu: []
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'hide'
+      },
+      {
+        role: 'hideothers'
+      },
+      {
+        role: 'unhide'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'quit'
+      }
+    ]
+  })
+  // Edit menu.
+  template[1].submenu.push(
+    {
+      type: 'separator'
+    },
+    {
+      label: 'Speech',
+      submenu: [
+        {
+          role: 'startspeaking'
+        },
+        {
+          role: 'stopspeaking'
+        }
+      ]
+    }
+  )
+  // Window menu.
+  template[3].submenu = [
+    {
+      label: 'Minimize',
+      accelerator: 'CmdOrCtrl+M',
+      role: 'minimize'
+    },
+    {
+      type: 'separator'
+    },
+    {
+      label: 'Bring All to Front',
+      role: 'front'
+    }
+  ]
+}
+
 function createWindow () {
   win = new BrowserWindow({ width: 1920, height: 1080, frame: false })
 
   win.loadURL(`file://${__dirname}/index.html`)
-  //win.webContents.openDevTools()
+  // win.webContents.openDevTools()
+
+  win.on('close', function (e) {
+    if (win.__recording) {
+      e.preventDefault()
+
+      dialog.showMessageBox(win, {
+        type: 'warning',
+        buttons: ['Yes', 'No'],
+        title: 'Confirm',
+        message: 'Recording is still on. Still want to exit?'
+      }, (confirm) => {
+        if (confirm === 0) {
+          win.onbeforeunload = null
+          app.exit()
+        }
+      })
+    }
+  })
 
   win.on('closed', function () {
     win = null
@@ -32,22 +178,10 @@ app.on('activate', function () {
 })
 
 function defaultMenu (win) {
-  const menu = new Menu()
+  win.__recording = false
+  const menu = Menu.buildFromTemplate(template)
 
-  menu.append(new MenuItem({
-    label: 'CodeCam',
-    submenu: [
-      {
-        label: 'Quit',
-        accelerator: 'CmdOrCtrl+Q',
-        click () {
-          app.exit()
-        }
-      }
-    ]
-  }))
-
-  menu.append(new MenuItem({
+  menu.insert(menu.items.length - 2, new MenuItem({
     label: 'Record',
     submenu: [
       {
@@ -65,32 +199,11 @@ function defaultMenu (win) {
 }
 
 function recordMenu (win) {
-  const menu = new Menu()
+  win.__recording = true
 
-  menu.append(new MenuItem({
-    label: 'CodeCam',
-    submenu: [
-      {
-        label: 'Quit',
-        accelerator: 'CmdOrCtrl+Q',
-        click () {
-          dialog.showMessageBox(win, {
-            type: 'warning',
-            buttons: ['Yes', 'No'],
-            title: 'Confirm',
-            message: 'Recording is still on. Still want to exit?'
-          }, (confirm) => {
-            if (confirm === 0) {
-              win.onbeforeunload = null
-              app.exit()
-            }
-          })
-        }
-      }
-    ]
-  }))
+  const menu = Menu.buildFromTemplate(template)
 
-  menu.append(new MenuItem({
+  menu.insert(menu.items.length - 2, new MenuItem({
     label: 'RECORDING...',
     submenu: [
       {
